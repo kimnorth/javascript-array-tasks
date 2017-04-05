afb77267b886c192b0cc37ae7d7624fed45ca713
kimnorth <benignhumour@gmail.com>
2017-04-05 18:38:35 +0100

    Completed concat test using two forEach loops in an anonymous function.

---
+++ array_tasks.js
@@ -0,0 +1,44 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		var resultingArray = [];
+		arr1.forEach(function(element){
+			resultingArray.push(element);
+		})
+		arr2.forEach(function(element){
+			resultingArray.push(element);
+		})
+		return resultingArray;
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+		
+	// },
+
+	// square: function (arr) {
+		
+	// },
+
+	// sum: function (arr) {
+		
+	// },
+
+	// findDuplicates: function (arr) {
+		
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+		
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+		
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+		
+	// }
+
+}
+
+module.exports = arrayTasks
